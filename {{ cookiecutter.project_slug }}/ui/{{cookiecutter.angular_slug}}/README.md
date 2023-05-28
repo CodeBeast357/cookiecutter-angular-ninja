c8a67e12b2b2df9973f5d434d7b388d2398c6aaa
Micheal Taylor <bubthegreat@gmail.com>
2023-05-27 17:04:49 -0700

    Initial UI template

--- {{ cookiecutter.project_slug }}/ui/{{cookiecutter.angular_slug}}/README.md
+++ {{ cookiecutter.project_slug }}/ui/{{cookiecutter.angular_slug}}/README.md
@@ -1,4 +1,4 @@
-# Cookiecutteruireplace
+# {{cookiecutter.angular_slug}}
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.3.
 
--- {{ cookiecutter.project_slug }}/ui/{{cookiecutter.angular_slug}}/src/app/app.component.spec.ts
+++ {{ cookiecutter.project_slug }}/ui/{{cookiecutter.angular_slug}}/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cookiecutteruireplace'`, () => {
+  it(`should have as title '{{cookiecutter.angular_slug}}'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cookiecutteruireplace');
+    expect(app.title).toEqual('{{cookiecutter.angular_slug}}');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cookiecutteruireplace app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('{{cookiecutter.angular_slug}} app is running!');
   });
 });
